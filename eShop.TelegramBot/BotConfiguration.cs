894ae5905910d435b5f547b62dfe2588dc8403f5
Vladimir Strizhakov <mrhugaa@gmail.com>
2023-04-22 12:40:35 +0300

    Add project files.

---
+++ eShop.TelegramBot/BotConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace eShop.TelegramBot
+{
+    public class BotConfiguration
+    {
+        public const string WebhookRoute = "/api/telegram/webhook";
+
+        public Uri Host { get; set; }
+        public string BotToken { get; set; }
+    }
+}
---
+++ eShop.TelegramBot/Controllers/WebhookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace eShop.TelegramBot.Controllers
+{
+    [Route(BotConfiguration.WebhookRoute)]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update, [FromServices] ITelegramBotClient botClient, CancellationToken cancellationToken)
+        {
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+                if (message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, message.Text, cancellationToken: cancellationToken);
+                }
+            }
+            return Ok();
+        }
+    }
+}

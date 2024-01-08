27cc261b5844e1cade917f23594e4dccf8b909df
Volodymyr Stryzhakov <vladimir.strizhakov@touchcast.com>
2024-01-08 23:04:48 +0200

    Responsive UI added to the Clients page

--- eshop/src/features/main/Clients.tsx
+++ eshop/src/features/main/Clients.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useActivateClientMutation, useDeactivateClientMutation, useGetClientsQuery } from "../api/distributionSlice";
-import { Badge, Button, Col, ListGroup, ListGroupItem, Row, Spinner, Stack } from "react-bootstrap";
+import { Badge, Button, Card, Col, ListGroup, ListGroupItem, Row, Spinner, Stack } from "react-bootstrap";
 import MyInvitation from "./MyInvitation";
 import { ReactComponent as Telegram } from "../../assets/telegram.svg";
 import { ReactComponent as Viber } from "../../assets/viber.svg";
@@ -22,53 +22,63 @@ const Clients: React.FC = () => {
                 {clientsLoading && (
                     <Spinner className="align-self-center my-2" />
                 )}
-                {clients && (
-                    <ListGroup>
-                        {clients.map(client => (
-                            <ListGroupItem key={client.id} className="py-2">
-                                <Row>
-                                    <Col className="d-flex align-items-center">
-                                        <strong>{client.firstName} {client.lastName}</strong>
-                                    </Col>
-                                    <Col xs="auto" className="d-flex align-items-center">
-                                        <Row>
-                                            <Col>
-                                                <Stack direction="horizontal" gap={1}>
-                                                    {client.viberChat && (
-                                                        <Viber fill="white" height="36px" />
-                                                    )}
-                                                    {client.telegramChats.length > 0 && (
-                                                        <div className="position-relative">
-                                                            <Telegram fill="white" height="36px" />
-                                                            <Badge
-                                                                className="position-absolute" 
-                                                                bg="info"
-                                                                style={{
-                                                                    bottom: -4,
-                                                                    right: -6,
-                                                                    fontSize: 10,
-                                                                    paddingTop: 2,
-                                                                    paddingBottom: 2,
-                                                                }}>
-                                                                {client.telegramChats.length}
-                                                            </Badge>
-                                                        </div>
-                                                    )}
-                                                </Stack>
-                                            </Col>
-                                        </Row>
-                                    </Col>
-                                    <Col xs="auto">
-                                        <Stack direction="horizontal" gap={3}>
-                                            <Button variant="outline-secondary" disabled={client.isActivated} onClick={() => activateClient(client.id)}>Активувати</Button>
-                                            <Button variant="outline-secondary" disabled={!client.isActivated} onClick={() => deactivateClient(client.id)}>Деактивувати</Button>
-                                        </Stack>
-                                    </Col>
-                                </Row>
-                            </ListGroupItem>
-                        ))}
-                    </ListGroup>
-                )}
+                <Row className="row-gap-3">
+                    {clients && clients.map(client => (
+                        <Col xxl={6}>
+                            <Card key={client.id}>
+                                <Card.Body>
+                                    <Row>
+                                        <Col xs={7} sm={8} md>
+                                            <Row>
+                                                <Col className="d-flex align-items-center">
+                                                    <strong>{client.firstName} {client.lastName}</strong>
+                                                </Col>
+                                                <Col md="auto" className="d-flex align-items-center">
+                                                    <Row>
+                                                        <Col>
+                                                            <Stack direction="horizontal" gap={1}>
+                                                                {client.viberChat && (
+                                                                    <Viber fill="white" height="36px" />
+                                                                )}
+                                                                {client.telegramChats.length > 0 && (
+                                                                    <div className="position-relative">
+                                                                        <Telegram fill="white" height="36px" />
+                                                                        <Badge
+                                                                            className="position-absolute"
+                                                                            bg="info"
+                                                                            style={{
+                                                                                bottom: -4,
+                                                                                right: -6,
+                                                                                fontSize: 10,
+                                                                                paddingTop: 2,
+                                                                                paddingBottom: 2,
+                                                                            }}>
+                                                                            {client.telegramChats.length}
+                                                                        </Badge>
+                                                                    </div>
+                                                                )}
+                                                            </Stack>
+                                                        </Col>
+                                                    </Row>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                        <Col xs={5} sm={4} md="auto">
+                                            <Row className="row-gap-1">
+                                                <Col md="auto">
+                                                    <Button className="w-100" variant="outline-secondary" disabled={client.isActivated} onClick={() => activateClient(client.id)}>Активувати</Button>
+                                                </Col>
+                                                <Col md="auto">
+                                                    <Button className="w-100" variant="outline-secondary" disabled={!client.isActivated} onClick={() => deactivateClient(client.id)}>Деактивувати</Button>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
             </Stack>
         </Stack>
     );
